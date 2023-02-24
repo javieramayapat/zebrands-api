1730f09b54edf35ec86da404bcf982b523109d19
Javier Amaya <Javieramayapat@gmail.com>
2023-02-24 13:42:23 -0600

    feat[products]: Add permision for crud and enable get products

--- products/views.py
+++ products/views.py
@@ -2,10 +2,11 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from products.api.serializers import ProductSerializer
 from products.models import Product
+from products.api.permissions import IsAdminOrReadOnly
 
 
 class ProductsModelViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-    http_method_names = ['post', 'patch', 'delete']
+    http_method_names = ['get','post', 'patch', 'delete']
