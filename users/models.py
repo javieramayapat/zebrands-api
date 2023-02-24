2b326b339f266f9942508ce52fc445c5b65914e1
Javier Amaya <Javieramayapat@gmail.com>
2023-02-24 13:26:28 -0600

    ref[users]: updated model to use username instead email

--- users/models.py
+++ users/models.py
@@ -3,7 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    pass
