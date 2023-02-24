b8ca0be83dbea2071c044635b69d71074768fed9
Javier Amaya <Javieramayapat@gmail.com>
2023-02-24 00:36:38 -0600

    feat[categories]: Add permisions for categories crud

--- categories/views.py
+++ categories/views.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
+from categories.api.permissions import IsAdminOrReadOnly
 
 
 class CategoryModelViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
     ordering = ['-created_at']
     queryset = Category.objects.all()
